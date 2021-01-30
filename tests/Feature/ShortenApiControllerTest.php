b10c0b1ab8a0714893b851033226ff6ef80c90c3
Eric Percifield <medalink7@gmail.com>
2021-01-29 19:00:17 -0600

    Add tests for creating/viewing short URLs.

---
+++ tests/Feature/ShortenApiControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Link;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShortenApiControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function a_user_can_shorten_a_url()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $testLink = Link::factory()->make();
+
+        $response = $this->put('/api/shorten-url', [
+            'url' => $testLink->url,
+        ]);
+
+        // Lookup result from DB
+        $link = Link::where('user_id', $user->id)->where('url', $testLink->url)->first();
+
+        // Verify our match
+        $response->assertJson([
+            'short_url' => $link->short_url,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_view_their_short_urls()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $testLink = Link::factory()->count(5)->create();
+
+        $response = $this->get('/api/view-urls');
+
+        // Verify we match our DB
+        $response->assertJson($testLink->toArray());
+    }
+}
